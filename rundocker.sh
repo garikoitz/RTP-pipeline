fce3d9e1ec6b1baadb2479721cace69fc222bf64
Garikoitz Lerma-Usabiaga <garikoitz@gmail.com>
2019-06-23 01:08:53 -0700

    Update rundocker.sh

--- rundocker.sh
+++ rundocker.sh
@@ -1,8 +1,8 @@
-dataDir="/black/localhome/glerma/TESTDATA/AFQ_PIPELINE"
+dataDir="/black/localhome/glerma/TESTDATA/RTP-pipeline"
 
 # docker run -ti --rm --entrypoint /bin/bash  \
 docker run -ti --rm  \
 	       -v $dataDir/input:/flywheel/v0/input  \
    	       -v $dataDir/output:/flywheel/v0/output  \
            -v $(pwd)/example_config.json:/flywheel/v0/config.json \
-   	       scitran/afq-pipeline:$1
+   	       vistalab/RTP-pipeline:$1
