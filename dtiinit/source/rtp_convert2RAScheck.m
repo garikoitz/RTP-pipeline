072025996faa3aacda866b5a815d1436ed3d53ae
garikoitz <garikoitz@gmail.com>
2019-12-23 23:39:52 -0800

    moved file from afq to dtiinit

---
+++ dtiinit/source/rtp_convert2RAScheck.m
@@ -0,0 +1,25 @@
+function [convert2RAS,dsline] = rtp_convert2RAScheck(fname)
+%RTP_CONVERT2RASCHECK Summary of this function goes here
+%   Detailed explanation goes here
+
+% fname = '/Users/glerma/soft/rtp-pipeline/local/DTIINIT/input/T1w.nii.gz';
+
+    convert2RAS     = false;
+    [status,result] = system(sprintf('mrinfo %s',fname));
+    if status ~= 0 
+        error("Mrtrix's mrinfo did return expected results, check if it installed properly")
+    end
+    
+    TextAsCells     = regexp(result, '\n', 'split');
+    dsline          = TextAsCells(contains(TextAsCells, "Data strides:"));
+    dsline          = dsline{1};
+    negs            = regexp(dsline, '-','once');
+    if ~isempty(negs);convert2RAS=true;end
+    dsline          = strrep(dsline,'Data strides:','');
+    dsline          = strrep(dsline,'[','');
+    dsline          = strrep(dsline,']','');
+    dsline          = strrep(dsline,' ','');
+    if (~strcmp(dsline,'123') || ~strcmp(dsline,'1234'));convert2RAS=true;end
+
+end
+
