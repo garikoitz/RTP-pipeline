74a9fb4b9967f8b158bdb66c0974f507fd025356
garikoitz <garikoitz@gmail.com>
2020-03-28 00:19:13 -0700

    Commiting before building :4.2.4

--- build.sh
+++ build.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 # Builds the gear/container
-# The container can be exported using the export.sh script
 # cd afq/source/bin
 # . ./compile.sh
-# cd ../../../dtiinit/source/
-# . ./compile.sh
 # cd ../../
-# git add .
-# git commit -m "Commiting before building $GEAR:$1"
+git add .
+git commit -m "Commiting before building $GEAR:$1"
 GEAR=vistalab/rtp-pipeline
 docker build --tag $GEAR:$1 .
